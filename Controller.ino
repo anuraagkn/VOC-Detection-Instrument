151b257d8714c9e91fbef11b8735648e9454555c
anuraagkn <anuraagnarang@mail.com>
2017-12-21 01:04:10 +0000

    Add files via upload

--- Controller.ino
+++ Controller.ino
@@ -1,15 +1,18 @@
+// instrument libraries
 #include <CommandHandler.h>
 #include <Menu.h>
 #include <Source.h>
 
-CommandHandler ch;
+CommandHandler ch;  // instantiate command handler object
 
+// this code runs once when starting the program
 void setup()
 {
-    ch.boot();
+    ch.boot();  // set the defaults for the instrument
 }
 
+// this code runs repeatedly
 void loop()
 {
-    ch.processCommand();
+    ch.processCommand();  // check for and process a command
 }

7b00a085d485e7c0386e057aec89a40ed3040217
anuraagkn <anuraagnarang@mail.com>
2018-02-21 13:45:55 +0000

    Create Controller.ino

---
+++ Back-End/Controller/Controller.ino
@@ -0,0 +1,18 @@
+// instrument libraries
+#include <CommandHandler.h>
+#include <Menu.h>
+#include <Source.h>
+
+CommandHandler ch;  // instantiate command handler object
+
+// this code runs once when starting the program
+void setup()
+{
+    ch.boot();  // set the defaults for the instrument
+}
+
+// this code runs repeatedly
+void loop()
+{
+    ch.processCommand();  // check for and process a command
+}
